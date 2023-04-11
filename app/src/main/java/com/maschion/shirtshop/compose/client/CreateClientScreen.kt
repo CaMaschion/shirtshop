85311e0aae7454ea2f5c6490b2c2ed10397d5b5a
camila <cm.maschion@gmail.com>
2023-04-11 12:40:56 +0100

    developing create client screen

--- app/src/main/java/com/maschion/shirtshop/compose/client/CreateClientScreen.kt
+++ app/src/main/java/com/maschion/shirtshop/compose/client/CreateClientScreen.kt
@@ -1,31 +1,50 @@
 package com.maschion.shirtshop.compose.client
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.KeyboardType
 
 @Composable
-fun CreateClient(modifier: Modifier = Modifier) {
-    var name by remember {
-        mutableStateOf("")
-    }
-
+fun CreateClient() {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TextField(
+        var name by remember {
+            mutableStateOf("Escreva aqui")
+        }
+
+        OutlinedTextField(
             value = name,
             onValueChange = { name = it },
             label = { Text("Name") },
-            singleLine = true
+            leadingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Filled.Person,
+                        contentDescription = "Person icon"
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(onClick = { Log.d("Clicado", "Clicado")
+                }) {
+                    Icon(imageVector = Icons.Filled.Check,
+                        contentDescription = "Check icon"
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Ascii
+            )
         )
 
     }
