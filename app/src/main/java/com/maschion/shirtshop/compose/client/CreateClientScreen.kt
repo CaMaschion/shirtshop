0fffc1b914d9d07ed9cf96c69287658f86b31157
camila <cm.maschion@gmail.com>
2023-04-11 12:03:09 +0100

    developing create client screen

--- app/src/main/java/com/maschion/shirtshop/compose/client/CreateClientScreen.kt
+++ app/src/main/java/com/maschion/shirtshop/compose/client/CreateClientScreen.kt
@@ -1,30 +1,32 @@
 package com.maschion.shirtshop.compose.client
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
 
 @Composable
-fun CreateClient() {
-    var teste by rememberSaveable(stateSaver = TextFieldValue.Saver) {
-        mutableStateOf(TextFieldValue("example", TextRange(0, 7)))
+fun CreateClient(modifier: Modifier = Modifier) {
+    var name by remember {
+        mutableStateOf("")
     }
 
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         TextField(
-            value = teste,
-            onValueChange = { teste = it },
-            label = { Text("Label") }
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Name") },
+            singleLine = true
         )
-        Text(text = "${teste.text}")
 
     }
 }
