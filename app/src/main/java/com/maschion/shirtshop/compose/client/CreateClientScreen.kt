8f5440d065fcbfc7bbc6a6a610b90d68f687e4cf
camila <cm.maschion@gmail.com>
2023-04-16 13:06:41 +0100

    developing create client screen

--- app/src/main/java/com/maschion/shirtshop/compose/client/CreateClientScreen.kt
+++ app/src/main/java/com/maschion/shirtshop/compose/client/CreateClientScreen.kt
@@ -1,41 +1,111 @@
 package com.maschion.shirtshop.compose.client
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CreateClient() {
+
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.padding(20.dp),
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        var name by remember {
-            mutableStateOf("Escreva aqui")
+        var clientName by remember {
+            mutableStateOf(TextFieldValue())
+        }
+        var clientPhone by remember {
+            mutableStateOf(TextFieldValue())
+        }
+        var clientEmail by remember {
+            mutableStateOf(TextFieldValue())
+        }
+        var clientBirthday by remember {
+            mutableStateOf(TextFieldValue())
+        }
+        var clientFabric by remember {
+            mutableStateOf(TextFieldValue())
         }
 
         OutlinedTextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text("Name") },
-            leadingIcon = {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(imageVector = Icons.Filled.Person,
-                        contentDescription = "Person icon"
-                    )
-                }
-            },
+            value = clientName,
+            onValueChange = { clientName = it },
+            label = { Text("Nome") },
             keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Ascii
+                keyboardType = KeyboardType.Text
             )
         )
 
+        Spacer(modifier = Modifier.height(15.dp))
+
+        OutlinedTextField(
+            value = clientPhone,
+            onValueChange = { clientPhone = it },
+            label = { Text("Telefone") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text
+            )
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        OutlinedTextField(
+            value = clientEmail,
+            onValueChange = { clientEmail = it },
+            label = { Text("E-mail") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text
+            )
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        OutlinedTextField(
+            value = clientBirthday,
+            onValueChange = { clientBirthday = it },
+            label = { Text("Data de Aniversário") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text
+            )
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        OutlinedTextField(
+            value = clientFabric,
+            onValueChange = { clientFabric = it },
+            label = { Text("Tecidos:") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text
+            )
+        )
+
+        Spacer(modifier = Modifier.height(200.dp))
+
+        Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+            Button(
+                onClick = {},
+                shape = RoundedCornerShape(50.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text(text = "Escolher camisa")
+            }
+        }
     }
 }
