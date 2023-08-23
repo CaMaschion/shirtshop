9b1744cc5b1557eb0c5bad09cf358004b26469ab
camila <cm.maschion@gmail.com>
2023-08-23 09:44:32 +0100

    add room

--- app/src/main/java/com/maschion/shirtshop/data/client/ClientDatabase.kt
+++ app/src/main/java/com/maschion/shirtshop/data/client/ClientDatabase.kt
@@ -2,9 +2,12 @@ package com.maschion.shirtshop.data.client
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.maschion.shirtshop.utils.Converters
 
 @Database(entities = [Client::class], version = 1, exportSchema = false)
-abstract class ClientDatabase: RoomDatabase(){
+@TypeConverters(Converters::class)
+abstract class ClientDatabase: RoomDatabase() {
 
     abstract fun client(): Client
 }
